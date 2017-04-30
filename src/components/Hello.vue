0f6884d6d2613b5de293e8d19c9d818337423374
马齐玲 <maqiling01@chinatopcredit.com>
2017-04-30 15:05:20 +0800

    非vuex父子组件的数据传递

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,6 +4,10 @@
     <Cart></Cart>
     <counter></counter>
     <Count></Count>
+    <p style="margin-top: 40px">父子组件通信</p>
+    <p>子组件传来的数据communication，父组件通过使用相同的名称communication进行监听，获取数据后进行相应的操作</p>
+    <p>子组件传递的数据：{{msg}}</p>
+    <InputItem item="用户名" val="请输入用户名" @communication="change"></InputItem>
   </div>
 
 </template>
@@ -13,6 +17,7 @@
   import ProductList from '@/components/productList';
   import Counter from './counter.vue';
   import Count from './count.vue';
+  import InputItem from './shared/inputItem.vue';
   export default {
     name: 'hello',
     data () {
@@ -24,7 +29,13 @@
       Cart,
       ProductList,
       Counter,
-      Count
+      Count,
+      InputItem
+    },
+    methods: {
+      change (msg) {
+        this.msg = msg;
+      }
     }
   };
 </script>
